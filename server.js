ba94278d6268ba0c3932a1a2715dcf74b8e79dfc
JorgeWS <jorgews.dev@gmail.com>
2024-01-28 20:17:27 -0300

    feat: 🎸 Criará repositório de API node para ser orquestrada no docker

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const production = true;
+
+const host = production ? "0.0.0.0" : "127.0.0.1";
+const port = 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.listen(port, host);
